530f492b60d9c2b203c5299ec092afb43e3facc0
Minh Nguyen <minhnq.0702@gmail.com>
2020-08-12 11:52:40 +0700

    add switch case test

--- testing-go/switch-case/main.go
+++ testing-go/switch-case/main.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	a := 2
+	switch a {
+	case 2:
+	case 3:
+	default:
+		fmt.Println("Invalid")
+	}
+
+	swithFallthourgh()
+}
+
+func swithFallthourgh() {
 	a := int64(1)
 
 	switch a {
@@ -11,7 +23,7 @@ func main() {
 	case 2:
 		fallthrough
 	case 3:
-		// do no thing
+		// Fall all to default
 		fallthrough
 	default:
 		fmt.Println("This is default!!!!")
