be5ef151588d8199d02ced3a8f81fc272a362ea3
MingNe <minhnq.0702@gmail.com>
2024-08-16 11:11:18 +0700

    feat: add test goroutine performance

---
+++ testing-go/go-coroutine/go.mod
@@ -0,0 +1,3 @@
+module go-coroutine
+
+go 1.22.5
---
+++ testing-go/go-coroutine/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var mut sync.Mutex
+
+func atomicCount(id int, counter *int32, wg *sync.WaitGroup) {
+	defer wg.Done() // * decrease wg by 1
+	// defer mut.Unlock()
+	// mut.Lock()
+	// *counter += 1
+	atomic.AddInt32(counter, 1)
+}
+
+func main() {
+	number_of_counter := 100000000
+	var counter int32 = 0
+	fmt.Printf("starting\n")
+	var wg sync.WaitGroup
+
+	wg.Add(number_of_counter) // * add number of task need to do into WG
+
+	start_at := time.Now()
+	for id := range number_of_counter {
+		go atomicCount(id, &counter, &wg)
+	}
+
+	wg.Wait() // * wait until WG count to zero
+	eslapsed := time.Since(start_at)
+	fmt.Printf("Done %d with %vms\n", counter, eslapsed.Milliseconds())
+}
