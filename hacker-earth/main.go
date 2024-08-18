841b28539343f7cafbc9dacff415f2d16d33164b
MingNe <minhnq.0702@gmail.com>
2024-08-18 21:07:26 +0700

    feat: test runner

--- hacker-earth/main.go
+++ hacker-earth/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "hacker-earth/easy"
+import (
+	"hacker-earth/easy"
+)
 
 func main() {
 	// easy.FavoriteSinger()
-	easy.Factorial(5)
+	// easy.Factorial(5)
+	easy.PalindromicString()
 }
