2535a7db682769f06da41993b567ee959680b269
Minh Nguyen <minhnq.dev@gmail.com>
2024-08-27 10:18:22 -0700

    refactor: remove unused code

--- testing-go/test-channel/main.go
+++ testing-go/test-channel/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"test-channel/testbasic"
 	"time"
 )
 
@@ -23,23 +23,18 @@ func battle(name string, done chan struct{}) {
 }
 
 func main() {
-	// testbasic.Channel_basic()
+	testbasic.Channel_basic()
 	lstOpp := []string{"Dog", "Cat", "Shark"}
-	var wg sync.WaitGroup
+
 	done := make(chan struct{})
 	for _, opp := range lstOpp {
 		// time.Sleep(1 * time.Second)
 		fmt.Printf("Before goroutine %s\n", opp)
-		wg.Add(1)
 		go battle(opp, done)
 		fmt.Printf("After goroutine %s\n", opp)
 
 	}
-	// wg.Wait()
-	// for i := 0; i < len(lstOpp); i++ {
-	// 	d := <-done
-	// 	fmt.Printf("Done with %v\n", d)
-	// }
+
 	count := 0
 	for {
 		fmt.Printf("Waiting for done %d\n", count)
